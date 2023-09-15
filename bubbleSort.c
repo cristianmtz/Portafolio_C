131463939a3f0542a82f967e86353c226595aee3
anthony <anthony@apto.mx>
2023-09-15 15:10:49 -0600

    bubblesort algorithm explanation

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,6 +1,3 @@
-/*Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order.
- This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.*/
-
 #include <stdio.h>
 
 void bubble_sort(int *array, int length);
