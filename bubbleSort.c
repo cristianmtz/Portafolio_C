0e34d1b77480528e0995144bf62068ca9d53efb2
anthony <anthony@apto.mx>
2023-09-15 14:41:27 -0600

    BubbleSort algorithm added

---
+++ bubbleSort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void bubble_sort(int *array, int length);
+
+int main()
+{
+    int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int length = sizeof(array) / sizeof(array[0]);
+
+    bubble_sort(array, length);
+
+    return 0;
+}
+
+void bubble_sort(int *array, int length)
+{
+
+    int temp = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < (length - 1); j++)
+        {
+
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < length; i++)
+    {
+        printf("array[%d] = %d\n", i, array[i]);
+    }
+}
